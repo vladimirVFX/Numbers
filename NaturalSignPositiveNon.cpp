99e30dee5129873092f5229ff62ab8b5ca9bbe05
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-13 18:24:40 +0300

    Add files via upload

---
+++ NaturalSignPositiveNon.cpp
@@ -0,0 +1,26 @@
+﻿// NaturalSignPositiveNon.cpp
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    //Define_variables
+    const int num = 0;
+    const int N = 5;
+
+    //Main_cycle
+    for (int i = 0; i < N; i++){
+        std::cout << i * 2 <<" ";
+        std::cout << i * 2 + 1 << " ";
+        std::cout << sin(i) << " ";
+        std::cout << cos(i) << " ";
+        std::cout << tan(i) << " ";
+        std::cout << asin(i) << " ";
+        std::cout << acos(i) << " ";
+        std::cout << atan(i) << "\n";
+    }
+}
